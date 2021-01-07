61d28eec04e4d5931a14c34fd264710c99538922
RoiFuries <roi.furies@gmail.com>
2021-01-07 01:56:18 -0600

    Day 01, part1

---
+++ Day_01/Day_1.js
@@ -0,0 +1,57 @@
+
+
+function getArea(length, width) {
+    let area  = length * width;  
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter =  length + length + width + width;
+    return perimeter;
+}
+
+
+
+console.log(getArea(3, 4.5));
+console.log(getPerimeter(3, 4.5));
+
+
+///////////////////////////////////////////
+
+// por que esto no sirve?
+
+// let length = 3;
+// let width = 5;
+
+// function getArea(length, width) {
+//     let area  = length * width;  
+//     return area;
+// }
+
+// function getPerimeter(length, width) {
+//     let perimeter =  length + length + width + width;
+//     return perimeter;
+// }
+
+
+
+// console.log(getArea());
+// console.log(getPerimeter());
+
+
+//// no entendí esto de abajo ¿Por qué  sale 24?
+
+function factorial(n) {
+    if (n <= 1) {
+        return 1;
+    }
+    
+    return n * factorial(n - 1);
+} 
+
+
+
+
+console.log(factorial(4))
+
+
