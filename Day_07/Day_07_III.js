145e8c1011e5460c3616fa3f5ded15a3ad88f2bb
RoiFuries <roi.furies@gmail.com>
2021-02-01 18:26:26 -0600

    day 07

---
+++ Day_07/Day_07_III.js
@@ -0,0 +1,13 @@
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+const re = new RegExp('\\d+', 'g');
